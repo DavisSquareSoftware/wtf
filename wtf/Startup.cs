ca0fd3f3cb9da3eac267e9d0770c5e6c4410b660
David Tenney <tenneyconsulting@gmail.com>
2020-05-25 17:35:54 -0400

    Add project files.

---
+++ wtf/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wtf.Startup))]
+namespace wtf
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
